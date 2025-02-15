caedf83a5bb350018fef8934838c1135b3019678
Smrtnyk <milankeser@outlook.de>
2025-02-15 14:48:08 +0100

    chore: setup vue monorepo

---
+++ packages/frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Welcome to Momentum!</h1>
+    <p>This is your simple fitness app.</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+#app {
+  text-align: center;
+  margin-top: 2rem;
+}
+</style>

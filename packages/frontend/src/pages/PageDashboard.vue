38c605aadf3d32efb8be5414d3e3091481f23209
Smrtnyk <milankeser@outlook.de>
2025-02-16 14:29:37 +0100

    chore: move workout logger to own component

--- packages/frontend/src/pages/PageDashboard.vue
+++ packages/frontend/src/pages/PageDashboard.vue
@@ -1,96 +1,9 @@
 <template>
   <div>
-    <h2>Log a New Workout</h2>
-    <form @submit.prevent="submitWorkout">
-      <div>
-        <ExcercisePicker />
-        <label for="exercise">Exercise:</label>
-        <IInput
-          id="exercise"
-          container-class="w-full max-w-sm"
-          v-model="exercise"
-          type="text"
-          required
-        />
-      </div>
-      <div>
-        <label for="sets">Sets:</label>
-        <IInput
-          id="sets"
-          container-class="w-full max-w-sm"
-          v-model.number="sets"
-          type="number"
-          min="1"
-          required
-        />
-      </div>
-      <div>
-        <label for="reps">Reps:</label>
-        <IInput
-          id="reps"
-          container-class="w-full max-w-sm"
-          v-model.number="reps"
-          type="number"
-          min="1"
-          required
-        />
-      </div>
-      <div>
-        <label for="notes">Notes:</label>
-        <ITextArea id="notes" v-model="notes" />
-      </div>
-      <RippleButton>Log workout</RippleButton>
-    </form>
+    <WorkoutLogger />
   </div>
 </template>
 
 <script setup lang="ts">
-import { Timestamp } from "firebase/firestore";
-import { ref } from "vue";
-
-import ExcercisePicker from "../components/ExcercisePicker.vue";
-import IInput from "../components/ui/IInput.vue";
-import ITextArea from "../components/ui/ITextArea.vue";
-import RippleButton from "../components/ui/RippleButton.vue";
-import { auth } from "../firebase";
-import { addWorkout } from "../services/workout";
-
-const exercise = ref("");
-const sets = ref(1);
-const reps = ref(1);
-const notes = ref("");
-
-async function submitWorkout(): Promise<void> {
-  if (!auth.currentUser) {
-    alert("You must be logged in to log a workout.");
-    return;
-  }
-
-  const workout = {
-    date: Timestamp.now(),
-    exercise: exercise.value,
-    notes: notes.value,
-    reps: reps.value,
-    sets: sets.value,
-    userId: auth.currentUser.uid,
-  };
-
-  try {
-    await addWorkout(workout);
-    alert("Workout logged successfully!");
-    // Reset form fields
-    exercise.value = "";
-    sets.value = 1;
-    reps.value = 1;
-    notes.value = "";
-  } catch (error: any) {
-    alert(`Error logging workout: ${error.message}`);
-  }
-}
+import WorkoutLogger from "../components/WorkoutLogger.vue";
 </script>
-
-<style scoped>
-form > div {
-  margin-bottom: 1rem;
-}
-</style>

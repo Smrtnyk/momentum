235d2e59e3e4a2f6bb570b47e1fba3fc35a959a9
Smrtnyk <milankeser@outlook.de>
2025-02-16 12:54:13 +0100

    chore: enable dark mode

--- packages/frontend/src/components/AuthForm.vue
+++ packages/frontend/src/components/AuthForm.vue
@@ -1,26 +1,46 @@
 <template>
   <div>
-    <h2>{{ isLogin ? "Login" : "Register" }}</h2>
-    <form @submit.prevent="handleSubmit">
-      <input v-model="email" type="email" placeholder="Email" required />
-      <input
-        v-model="password"
-        type="password"
-        placeholder="Password"
-        required
-      />
-      <button type="submit">{{ isLogin ? "Login" : "Register" }}</button>
-    </form>
-    <button @click="toggleMode">
-      Switch to {{ isLogin ? "Register" : "Login" }}
-    </button>
+    <AuroraBackground>
+      <Motion
+        as="div"
+        :initial="{ opacity: 0, y: 40, filter: 'blur(10px)' }"
+        :in-view="{
+          opacity: 1,
+          y: 0,
+          filter: 'blur(0px)',
+        }"
+        :transition="{
+          delay: 0.3,
+          duration: 0.8,
+          ease: 'easeInOut',
+        }"
+        class="relative flex flex-col items-center justify-center gap-4 px-4"
+      >
+        <h2>{{ isLogin ? "Login" : "Register" }}</h2>
+        <form @submit.prevent="handleSubmit">
+          <input v-model="email" type="email" placeholder="Email" required />
+          <input
+            v-model="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <button type="submit">{{ isLogin ? "Login" : "Register" }}</button>
+        </form>
+        <button @click="toggleMode">
+          Switch to {{ isLogin ? "Register" : "Login" }}
+        </button>
+      </Motion>
+    </AuroraBackground>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Motion } from "motion-v";
 import { ref } from "vue";
 
 import { loginUser, registerUser } from "../services/auth";
+import AuroraBackground from "./AuroraBackground.vue";
 
 const isLogin = ref(true);
 const email = ref("");

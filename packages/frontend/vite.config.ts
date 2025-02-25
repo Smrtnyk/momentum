f8e162b862a0923c5c67b8aa252c73f2ae1344df
Smrtnyk <milankeser@outlook.de>
2025-02-25 19:49:04 +0100

    chore: add pwa

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import vuetify from "vite-plugin-vuetify";
 
 export default defineConfig({
@@ -7,6 +8,28 @@ export default defineConfig({
         vue(),
         vuetify({
             autoImport: true,
+        }),
+        VitePWA({
+            includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+            manifest: {
+                description: "Momentum Fitness App",
+                icons: [
+                    {
+                        sizes: "192x192",
+                        src: "icon-192x192.png",
+                        type: "image/png",
+                    },
+                    {
+                        sizes: "512x512",
+                        src: "icon-512x512.png",
+                        type: "image/png",
+                    },
+                ],
+                name: "Momentum",
+                short_name: "Momentum",
+                theme_color: "#27293D",
+            },
+            registerType: "autoUpdate",
         }),
     ],
 });
